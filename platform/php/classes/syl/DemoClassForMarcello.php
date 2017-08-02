1983e71478171e12293e8adf3c2eaee6b854cd20
DESKTOP-PS9K49A\Marcel <marcel.arioli@zhdk.ch>
2017-08-02 12:50:26 +0200

    2 new methods: calculate sum and analyse data

--- platform/php/classes/syl/DemoClassForMarcello.php
+++ platform/php/classes/syl/DemoClassForMarcello.php
@@ -16,8 +16,6 @@ namespace SaveYourLanguage\Statistics;
 // Shorthand for DatabaseController by namespace
 use SaveYourLanguage\Database\DatabaseController;
 
-// Include db connection
-require_once 'DatabaseConnection.php';
 require_once 'platform/php/classes/db/DatabaseController.php';  
 
 
@@ -33,25 +31,26 @@ class DemoClassForMarcello
     }
     
     // We asume that there are 3 status for soundfiles: fresh -> no transkritipons, unsolved->more transkritipons required, solved...well, solved
-    public function Get_Soundfile($tablename)
+	// (i used a table with ID, status (=fresh,unsolved or solved), path(example: https://uniserver/soundfiles/file00001))
+    public function getSoundfile($tableName)
     {
       
 		$link = DatabaseController::connect();
 		$dc = new DatabaseController($link);
 		
-		$soundfile = $dc->getRow('$tablename', array(
-			'status' => unsolved
+		$soundfile = $dc->getRow('$tableName', array(
+			'status' => 'unsolved'
 		));
-		if($soundfile==NULL){
-			  $soundfile = $dc->getRow('$tablename', array(
-			'status' => fresh
+		if($soundfile == null){
+			  $soundfile = $dc->getRow('$tableName', array(
+			'status' => 'fresh'
 			));		
 		}
 		// Close database
 		DatabaseController::disconnect($link);
 		unset($link);
 		
-		if($soundfile==NULL){
+		if($soundfile == null){
 			//no unsolved nor fresh files in database, return null?
 			return null;
 		}else{		
@@ -61,30 +60,43 @@ class DemoClassForMarcello
 		}		
     }
 	
-	//------------------------------------------------------------------------what kind of functions do we need?
+	//this method compares sums of all transkriptions of a soundFile and updates the accuracy for that transcription
+	public function AnalyseData($tableName, $fileIndex)
+	{
+		//get all transkriptions of the same file
+		$datarows = array();
+		$datarows = $dc->getRows('$tableName', array(
+			'fileIndex' => $fileIndex
+		));
+		
+		$averagevalue = 0;
+		foreach($datarows as $value)
+		{
+			$averagevalue = $averagevalue + $value['sum'];
+		}
+		
+		foreach($datarows as $value)
+		{
+			$accuracy = 0;
+			if($averagevalue < $value['sum']){
+				$accuracy = 100*($averagevalue/$value['sum']);
+			}else{
+				$accuracy = 100*($value['sum']/$averagevalue);
+			}
+			$dc->updateRow('$tableName', array(
+			'evaluation' => $accuracy
+			));
+		}		
+	}	
 	
-	//get audiofile --> store in variable for replay 
-	// Include library files
-    
-	
-	//play (replay) audio file
-	
-	//type in input -->add char to varchar-string
-	
-	//display string with blinking line at end 
-	
-	//correct input with back key
-	
-	//send data  (int audiofile_id, string data, int user_id)
-	
-	//get statistics
-	
-	//modify statistics
-	
-	//calculate precision (wasn't this uni work?!)
-	
-	//calculate star size and appearance
-	
-	//
-	
+	//takes a string and returns a sum of its characters
+	public function CalculateSum($dataString)
+	{
+		$dataString = strtolower ($dataString); //makes everything lowercase, usefull because a and A have very different asci values.
+		$sum = 0;
+		for ($i = 0; $i < strlen($dataString); $i++){
+			$sum = $sum + ord ($dataString[$i])-96; //adding the asci-value of each char. asci-value of a = 97, thats why substraction by 96 needs to hapen, otherwise failures aren't remarkable.
+		}
+		return $sum;
+	}
 }
