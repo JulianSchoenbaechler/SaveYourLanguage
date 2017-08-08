6e7548c2f87f4dd24ed6f54055bb93811957c304
DESKTOP-PS9K49A\Marcel <marcel.arioli@zhdk.ch>
2017-08-09 00:16:53 +0200

    minor stuff, new function playerAccuracy

--- platform/php/classes/syl/Statistics.php
+++ platform/php/classes/syl/Statistics.php
@@ -19,10 +19,8 @@ require_once dirname(__FILE__).'/../db/DatabaseController.php';
 
 use SaveYourLanguage\Database\DatabaseController;
 
-class DemoClassForMarcello
-{
-    
-    
+class Statistics
+{    
     // Constructor
     public function __construct()
     {
@@ -32,19 +30,22 @@ class DemoClassForMarcello
     // We asume that there are 3 status for soundfiles: fresh -> no transkritipons, unsolved->more transkritipons required, solved...well, solved
 	// (i used a table with ID, status (=fresh,unsolved or solved), path(example: https://uniserver/soundfiles/file00001))
     public function getSoundfile($tableName)
-    {
-      
+    {      
 		$link = DatabaseController::connect();
 		$dc = new DatabaseController($link);
 		
 		$soundfile = $dc->getRow('$tableName', array(
 			'status' => 'unsolved'
 		));
+		
 		if($soundfile == null){
-			  $soundfile = $dc->getRow('$tableName', array(
+			
+			$soundfile = $dc->getRow('$tableName', array(
 			'status' => 'fresh'
-			));		
+			));
+			  
 		}
+		
 		// Close database
 		DatabaseController::disconnect($link);
 		unset($link);
@@ -60,20 +61,23 @@ class DemoClassForMarcello
     }
 	
 	//this method compares sums of all transkriptions of a soundFile and updates the accuracy for that transcription
-	public function AnalyseData($tableName, $fileIndex)
+	public function analyseData($tableName, $fileIndex)
 	{
 		$link = DatabaseController::connect();
 		$dc = new DatabaseController($link);
+		
 		//get all transkriptions of the same file
 		$datarows = array();
 		$datarows = $dc->getRows($tableName, array(
 			'snippedid' => $fileIndex
 		));
+		
 		//first check if number of transkriptions was reached:
 		if(sizeof($datarows)<5){ // 5 is temporary threshold -> global constant?
 			return; //didn't reach threshold, don't analyse anything.
 		}
 		
+		//calculate the average of that soundsnipped.
 		$averagevalue = 0;
 		$counter = 0;
 		foreach($datarows as $value)
@@ -82,6 +86,8 @@ class DemoClassForMarcello
 			$counter = $counter + 1;
 		}
 		$averagevalue = (float)$averagevalue/(float)$counter;
+		
+		//calculate the percentage with the average for each transkription of that snipped.
 		foreach($datarows as $value)
 		{
 			$accuracy = 0;
@@ -92,28 +98,74 @@ class DemoClassForMarcello
 				$accuracy = 100*((float)$value['sum']/(float)$averagevalue);
 			}
 			//update row needs 2 arrays, first the values, then the conditions.
-			$dc->updateRow($tableName, array('evaluation' => (int)$accuracy),array('snippedid'=>$fileIndex,'sum'=>$value['sum']));			
+			$dc->updateRow($tableName, array(
+				'evaluation' => (int)$accuracy,
+				'status' => 'done'
+			), array(
+				'snippedid'=>$fileIndex,'sum'=>$value['sum']
+			));			
 		}
 		
 		//since those rows only get updated when threshold was reached, this snipped is done. we need to change the status in the soundfile database.
-		$dc->updateRow('soundfiles',array('status' => 'done'),array('id'=>$fileIndex));
+		$dc->updateRow('soundfiles',array(
+			'status' => 'done'
+		),array(
+			'id' => $fileIndex
+		));
+		
 		// Close database
 		DatabaseController::disconnect($link);
 		unset($link);
 	}	
 	
 	//takes a string and returns a sum of its characters
-	public function CalculateSum($dataString)
+	public function calculateSum($dataString)
 	{
-		$dataString = strtolower ($dataString); //makes everything lowercase, usefull because a and A have very different asci values.
+		//makes everything lowercase, usefull because a and A have very different asci values.
+		$dataString = strtolower ($dataString); 
 		$sum = 0;
+		
 		for ($i = 0; $i < strlen($dataString); $i++){
+			
 			$value = ord ($dataString[$i])-96;
+			
 			if($value < 0){
 				$value = 0;
 			}
-			$sum = $sum + $value; //adding the asci-value of each char. asci-value of a = 97, thats why substraction by 96 needs to hapen, otherwise failures aren't remarkable.
+			
+			//adding the asci-value of each char. asci-value of a = 97, thats why substraction by 96 needs to hapen, otherwise failures aren't remarkable.
+			$sum = $sum + $value; 
 		}
+		
 		return $sum;
+	}
+	
+	//a function that returns an integer between 0-100 that represents the players accuracy of the last timeperiod (of the starfield)
+	public function playerAccuracy($playerID)
+	{
+		//get all transkriptions of the player (if the table gets reseted each week, this is kind of weekly accuracy)
+		$datarows = array();
+		$datarows = $dc->getRows($tableName, array(
+			'playerid' => $playerID
+		));
+		
+		$averagevalue = 0;
+		$counter = 0;
+		
+		foreach($datarows as $value)
+		{
+			//we only take evaluated transkriptions into consideration. 
+		    if($value['status'] === 'done' && $value['evaluation'] > 0){
+				
+			    $averagevalue = $averagevalue + $value['evaluation'];
+			    $counter = $counter + 1;
+				
+			}
+		}
+		
+		//the averagevalue is the average accuracy of all datarows of that player.
+		$averagevalue = (float)$averagevalue/(float)$counter;
+		
+		return $averagevalue;
 	}
 }
